163bdeb5724d98db644eff60e02d547edcd2fae8
Evan Armstrong <earmstrong@lcs.on.ca>
2021-05-04 14:04:45 -0400

    Initial Commit

---
+++ OECulminating/OECulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OECulminatingApp.swift
+//  OECulminating
+//
+//  Created by Evan Armstrong on 2021-05-04.
+//
+
+import SwiftUI
+
+@main
+struct OECulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
