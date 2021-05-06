7d49e9a68c77afdfbf3ccd08130a2973f62122df
Evan Armstrong <earmstrong@lcs.on.ca>
2021-05-05 22:31:42 -0400

    Added more files for the tornadoes impacts

---
+++ OECulminating/Views/EconomicImpacts.swift
@@ -0,0 +1,20 @@
+//
+//  EconomicImpacts.swift
+//  OECulminating
+//
+//  Created by Evan Armstrong on 2021-05-05.
+//
+
+import SwiftUI
+
+struct EconomicImpacts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EconomicImpacts_Previews: PreviewProvider {
+    static var previews: some View {
+        EconomicImpacts()
+    }
+}
---
+++ OECulminating/Views/HumanImpacts.swift
@@ -0,0 +1,20 @@
+//
+//  HumanImpacts.swift
+//  OECulminating
+//
+//  Created by Evan Armstrong on 2021-05-05.
+//
+
+import SwiftUI
+
+struct HumanImpacts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HumanImpacts_Previews: PreviewProvider {
+    static var previews: some View {
+        HumanImpacts()
+    }
+}
